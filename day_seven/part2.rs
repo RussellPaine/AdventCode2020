81fd7b7a96b7c40c63db2602f04a32a4ad8d5e66
RussellPaine <russell.d.paine@gmail.com>
2020-12-07 17:46:36 +0000

    OPTI

--- day_seven/part2.rs
+++ day_seven/part2.rs
@@ -4,22 +4,8 @@ fn main() {
 
     let data = fs::read_to_string("input.in").expect("Unable to read file");
     let lines: Vec<&str> = data.split("\n").collect();
-    let mut count = 0;
 
-    for line in &lines {
-        let bags: Vec<&str> = line.split(" bags contain").collect();
-        if bags[0].contains("shiny gold") {
-            let bags2: Vec<&str> = bags[1].split(",").collect();
-            for bag in bags2 {
-                let elem: Vec<&str>  = bag.split_whitespace().collect();
-                let mut name: String = elem[1].to_string();
-                name.push_str(" ");
-                name.push_str(&elem[2].to_string());
-                count += elem[0].parse::<i32>().unwrap() + find_and_count(name, elem[0].parse::<i32>().unwrap(), &lines);
-            }
-        }
-    }
-    println!("{}", count);
+    println!("{}", find_and_count("shiny gold".to_string(), 1, &lines));
 }
 
 fn find_and_count(search: String, multiplier: i32, lines: &Vec<&str>) -> i32 {
